2aa169d39c90a2d4a5bd20f33506547faf53ee3e
Casper Koning <casper.e.koning@gmail.com>
2016-02-28 18:40:11 +0100

    Reduce time to live for temporary files

--- clean-tmp.sh
+++ clean-tmp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-find /Users/cko20685/tmp -mindepth 1 -mtime +14 -delete >> /Users/cko20685/logs/clean-up.txt
+find /Users/cko20685/tmp -mindepth 1 -mtime +5 -delete >> /Users/cko20685/logs/clean-up.txt
 
